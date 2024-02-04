a1c7dbf5df679205297083880bcdc1858b7d4419
ifruts <158855344+ifruts@users.noreply.github.com>
2024-02-04 20:02:12 -0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,9 @@
+var data = 
+'CNPJ› 37.085.118/0001-70<br>'+
+'FONE› (83) 9.8661-2160<br>' +
+'END.› CEASA, Campina Grande - Bloco B, Box 21';
+
+var description = 'Serviço de Cupom Fiscal para administradores da loja física Brasileiros Hortifruti.';
+var title = 'Brasileiros Hortifruti - ADM';
+
+var website = 'hortifrutibrasileiros';
