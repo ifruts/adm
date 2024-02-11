35c73a878ef4c7e94ff851afabf06abb9f12d2bd
ifruts <158855344+ifruts@users.noreply.github.com>
2024-02-11 01:23:22 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -36,3 +36,17 @@ self.addEventListener('fetch', (event) => {
     })
   );
 });
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+      caches.keys().then(function(cacheNames) {
+          return Promise.all(
+              cacheNames.filter(function(cacheName) {
+                  return true;
+              }).map(function(cacheName) {
+                  return caches.delete(cacheName);
+              })
+          );
+      })
+  );
+});
